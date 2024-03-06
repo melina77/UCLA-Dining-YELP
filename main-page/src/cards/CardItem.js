f615e7d7c67f5d805dc4af9c86361684b23660af
22sunm50 <lu.yingshan@gmail.com>
2024-03-06 09:11:02 -0800

    Sorry I forgot to uncomment my CardItem function

--- main-page/src/cards/CardItem.js
+++ main-page/src/cards/CardItem.js
@@ -3,49 +3,49 @@
 import React, { useState, useEffect } from 'react';
 import LikeButton from '../likes/LikeButton';
 
-// function CardItem(props) {
+function CardItem(props) {
 
-//     // Example handleAddFoodClick implementation
-//     const handleAddFoodClick = () => {
-//         // Assuming you have a way to determine the calorie count of this item
-//         const caloriesFromFood = 100; // Placeholder value
-//         props.onAddCalories(caloriesFromFood); // onAddCalories would be a function passed as a prop from the parent component
-//     };
+    // Example handleAddFoodClick implementation
+    const handleAddFoodClick = () => {
+        // Assuming you have a way to determine the calorie count of this item
+        const caloriesFromFood = 100; // Placeholder value
+        props.onAddCalories(caloriesFromFood); // onAddCalories would be a function passed as a prop from the parent component
+    };
     
-//     // Example handleOpenCommentsClick implementation
-//     const handleOpenCommentsClick = () => {
-//         // Assuming you manage the comments visibility and possibly the current item ID in a parent component
-//         props.onOpenComments(true, props.itemId); 
-//     };
+    // Example handleOpenCommentsClick implementation
+    const handleOpenCommentsClick = () => {
+        // Assuming you manage the comments visibility and possibly the current item ID in a parent component
+        props.onOpenComments(true, props.itemId); 
+    };
   
-//     return (
-//         <>
-//         <li className='cards__item'>
-//             <div className='cards__item__link' to={props.path}>
-//             <figure className='cards__item__wrap-dining_name' data-category={props.dining_name}>
-//                 <img
-//                 className='cards__item__img'
-//                 alt='Travel Image'
-//                 src={props.src}
-//                 />
-//             </figure>
-//             <div className='cards__item__caption'>
-//                 <h5 className='cards__item__caption_text'>Name: {props.name}</h5>
-//                 <h5 className='cards__item__caption_text'>Description: {props.description}</h5>
-//                 <h5 className='cards__item__caption_text'>Calorie Count: {props.calories}</h5>
-//                 <div className='cards__item__buttons'>
-//                     <button onClick={handleAddFoodClick}>Add Food to Calories</button>
-//                     <button onClick={handleOpenCommentsClick}>Open or Add Comments</button>
-//                     <LikeButton />
-//                 </div>
-//             </div>
-//             </div>
-//         </li>
-//         </>
-//     );
-// }
+    return (
+        <>
+        <li className='cards__item'>
+            <div className='cards__item__link' to={props.path}>
+            <figure className='cards__item__wrap-dining_name' data-category={props.dining_name}>
+                <img
+                className='cards__item__img'
+                alt='Travel Image'
+                src={props.src}
+                />
+            </figure>
+            <div className='cards__item__caption'>
+                <h5 className='cards__item__caption_text'>Name: {props.name}</h5>
+                <h5 className='cards__item__caption_text'>Description: {props.description}</h5>
+                <h5 className='cards__item__caption_text'>Calorie Count: {props.calories}</h5>
+                <div className='cards__item__buttons'>
+                    <button onClick={handleAddFoodClick}>Add Food to Calories</button>
+                    <button onClick={handleOpenCommentsClick}>Open or Add Comments</button>
+                    <LikeButton />
+                </div>
+            </div>
+            </div>
+        </li>
+        </>
+    );
+}
 
-// export default CardItem;
+export default CardItem;
 
 // 🌸🌸🌸🌸🌸🌸🌸🌸🌸🌸🌸🌸🌸🌸🌸🌸🌸🌸🌸🌸 WITH FETCH API 🌸🌸🌸🌸🌸🌸🌸🌸🌸🌸🌸🌸🌸🌸🌸🌸🌸🌸🌸🌸
 
