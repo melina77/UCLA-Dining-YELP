fee8f5085247970ff30e31391fa55fc17de92505
Phuc Truong <ptruong9090@gmail.com>
2024-03-05 22:41:06 -0800

    merged login page

--- Backend/server.js
+++
@@ -1,9 +0,0 @@
-const express = require('express')
-cors = require('cors')
-
-const API_PORT = process.env.PORT || 5000;
-const app = express();
-
-app.use(cors());
-
-app.listen(API_PORT, () => console.log(`Listening on port ${API_PORT}`));
