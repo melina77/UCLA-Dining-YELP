e1e11f1462fb60555a8ca0a75fbbe8b96b7b909d
Lucas <lucasthai2005@gmail.com>
2024-03-07 13:46:05 -0800

    Made minor changes to calcounter router
    Fixed bugs with calcounter router in order for its association
    with elements in the posts database to work when making get
    requests.

--- server/routes/calcounter.js
+++ server/routes/calcounter.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const { students, calcounter, food } = require("../models");
+const { students, calcounter } = require("../models");
 const sequelize = require("sequelize");
 const { validate } = require("./auth");
 
