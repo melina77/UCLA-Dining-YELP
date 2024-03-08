52e1d08e852a85053a8547b443d19b828160fbaa
Melina Eftekhari <mathgiraffe77@gmail.com>
2024-03-08 02:23:58 -0800

    Implmeneted search feature

--- src/App.js
+++ src/App.js
@@ -2,18 +2,41 @@ import React, { useState, useEffect } from 'react';
 //import axios from 'axios';
 //import { Link } from 'react-router-dom'
 import './nav.css';
-import Search from './search/search_react.js';
+import Search from './search/App.js';
 
 function Header() {
+  const [users, setUsers] = useState([]);
+  const [searchTerm, setSearchTerm] = useState('');
+
+  useEffect(() => {
+      fetch("https://jsonplaceholder.typicode.com/users")
+          .then(res => res.json())
+          .then(data => {
+              setUsers(data.map(user => ({
+                  name: user.name,
+                  email: user.email,
+                  id: user.id
+              })));
+          })
+          .catch(error => console.error('Error fetching users:', error));
+  }, []);
+
+  const handleSearch = (e) => {
+      const value = e.target.value.toLowerCase();
+      setSearchTerm(value);
+  };
+
   const handleLogout = () => {
     console.log('logged out')
   }
+
   return (
     <div className='header-container'>
       <header>
         <div className="logo-container">
           <img src="/bruingrub-high-resolution-logo-transparent.png" alt="Logo" className="logo" />
         </div>
+        <input type="text" placeholder="Search users" onChange={handleSearch} />
         <nav>
           <a to="/" className="nav-link">Home</a>
           <a to="/calorie-counter" className="nav-link">Calorie Counter</a>
@@ -21,7 +44,15 @@ function Header() {
           <button id='logout-button' onClick={handleLogout}>Logout</button>   
         </nav>
       </header>
-    </div>
+      <div>
+          {users.map(user => (
+            <div key={user.id} className={user.name.toLowerCase().includes(searchTerm) || user.email.toLowerCase().includes(searchTerm) ? '' : 'hide'}>
+              <h3>{user.name}</h3>
+              <p>{user.email}</p>
+            </div>
+          ))}
+      </div>
+ </div>
   )
 }
 
@@ -32,7 +63,6 @@ function CalorieCounter() {
 
   // State to manage input value
   const [inputValue, setInputValue] = useState('');
-
   // State to manage add food button
   const [isEditing, setisEditing] = useState(false);
 
@@ -138,7 +168,7 @@ export default function App() {
       <body>
         <Header />
         {/* <Search /> */}
-        <CalorieCounter />
+        {/* <CalorieCounter /> */}
       </body>
   )
 }
