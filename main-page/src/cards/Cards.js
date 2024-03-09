70a699add8d728c3a714b738dac3b910c0d48593
22sunm50 <lu.yingshan@gmail.com>
2024-03-08 17:15:51 -0800

    Added CORS and method/headers for the fetch login

--- main-page/src/cards/Cards.js
+++ main-page/src/cards/Cards.js
@@ -90,7 +90,10 @@ function Cards() {
         const fetchCardsData = async () => {
             try {
                 const response = await fetch('http://localhost:8080/f/', {
-                    method: 'GET'
+                    method: 'GET',
+                    headers: {
+                        'Content-Type': 'application/json'
+                    }
                 });
                 const data = await response.json();
                 setCardsData(data);
--- server/index.js
+++ server/index.js
@@ -1,7 +1,10 @@
 const express = require('express')
 const app = express();
 const db = require('./models');
+const cors = require('cors');
 require('dotenv').config();
+
+app.use(cors());
 
 app.use(express.json());
 const userRouter = require("./routes/users");
