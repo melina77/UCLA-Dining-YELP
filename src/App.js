fd78facfd7933a749bda3345102bc70e16ea8425
Phuc Truong <ptruong9090@gmail.com>
2024-03-10 21:39:46 -0700

    fixed fetch requests to return json

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import {jwtDecode} from 'jwt-decode';
 
 
 import Header from './global-header/header.js';
+import Header_Dining from './global-header/header2.js';
 import CalorieCounter from './counter-page/counter.js';
 import HomePage from './main-page/src/App.js';
 import LoginPage from './login-react/src/App.js';
@@ -11,18 +12,39 @@ import CommentsPage from './comments-page/src/App.js';
 import PostPage from './post-page-new/src/App.js';
 import SearchPage from './search-page/src/App.js';
 
+export default function App() {
+  const [userType, setUserType] = useState(null);
 
-function PageRoutes() {
+  return (
+    <Router>
+      {userType === null ? (
+        <LoginPage setUserType={setUserType} /> // Pass setUserType function to LoginPage
+      ) : (
+        <>
+          {userType === 'student' ? (
+            <Header /> // Render student header
+          ) : (
+            <Header_Dining /> // Render dining hall header
+          )}
+          <PageRoutes userType={userType} />
+        </>
+      )}
+    </Router>
+  );
+}
+
+
+function PageRoutes({userType}) {
   const location = useLocation();
-  const hideHeader = location.pathname === '/';
-
   const navigate = useNavigate();
-
+  
   const checkAuthToken = () => {
     const token = localStorage.getItem('authToken'); 
     let decodedToken;
+
     if (token) {
       decodedToken = jwtDecode(token);
+      console.log(decodedToken)
     }
     const currentTime = Math.floor(Date.now() / 1000);
     
@@ -32,7 +54,6 @@ function PageRoutes() {
     }
 
     // Check if token is expired 
-    
     else if (decodedToken !== undefined && decodedToken.exp < currentTime && location.pathname !== '/') {
       // Remove token and navigate back to login page
       localStorage.removeItem('authToken');
@@ -50,24 +71,26 @@ function PageRoutes() {
 
   return (
     <div>
-      {!hideHeader && <Header />}
       <Routes>
         <Route path="/" element={<LoginPage />} />
-        <Route path="/search" element={<SearchPage />} />
-        <Route path="/home" element={<HomePage />} />
-        <Route path="/calorie-counter" element={<CalorieCounter />} />
-        <Route path="/comments" element={<CommentsPage />} /> 
-        <Route path="/posts" element={<PostPage />} /> 
+        {userType === 'student' && (
+          <>
+            <Route path="/search" element={<SearchPage />} />
+            <Route path="/home" element={<HomePage />} />
+            <Route path="/calorie-counter" element={<CalorieCounter />} />
+            <Route path="/comments" element={<CommentsPage />} />
+          </>
+        )}
+        {userType === 'dining hall' && (
+          <>
+            <Route path="/search" element={<SearchPage />} />
+            <Route path="/home" element={<HomePage />} />
+            <Route path="/calorie-counter" element={<CalorieCounter />} />
+            <Route path="/comments" element={<CommentsPage />} />
+            <Route path="/posts" element={<PostPage />} />
+          </>
+        )}
       </Routes>
     </div>
-  );
-}
-
-export default function App() {
-
-  return (
-    <Router>
-      <PageRoutes />
-    </Router>
   );
 }
